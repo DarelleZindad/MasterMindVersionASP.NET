fc59da2a6d3bd49cc14fd65c397ddb451bc8c4e1
DarelleZindad <86950090+DarelleZindad@users.noreply.github.com>
2023-02-22 07:25:55 +0100

    fixing failures

--- wwwroot/javascript/JavaScript.js
+++ wwwroot/javascript/JavaScript.js
@@ -1,20 +1,20 @@
 ﻿"use strict";
 
 //variables needed
-let arrColors = ["gold", "yellowgreen", "darkorange", "cyan", "blueviolet",
+/*let arrColors = ["gold", "yellowgreen", "darkorange", "cyan", "blueviolet",
     "darkgray", "white", "darkred", "blue", "magenta", "steelblue", "tan",
     "lightseagreen", "yellow", "chartreuse", "indianred", "khaki",
     "dimgray", "darkgreen", "green", "mediumorchid", "violet",
     "limegreen", "papayawhip", "lavender", "lightcyan"];
 
-//let colorCount;
-//let positionCount;
-//let doubles;
-//let guesser;
+let colorCount;
+let positionCount;
+let doubles;
+let guesser;
 let codeToGuess = [];
 let guessedCode = [];
-//let usedColors = [];
-//let positionInOwnCode = 0;
+let usedColors = [];
+let positionInOwnCode = 0;
 let guess = 0;
 let allGuessedCodes = [];
 allGuessedCodes[0] = [];
@@ -23,12 +23,12 @@ let white = 0;
 let doubleguesses = 0;
 let impossibleCodes = 0;
 let autosolve = false;
-//let allPossibleCodes = [];
+let allPossibleCodes = [];
 let rand;
-//let maxPossibleCodes;
+let maxPossibleCodes;
 let bgcl = $("#idCode").css("background-color");
-//let maxCodesToBruteForce = 90000;
-
+let maxCodesToBruteForce = 90000;
+*/
 
 
 
